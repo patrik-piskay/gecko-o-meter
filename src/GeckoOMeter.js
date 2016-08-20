6362ab4c81ff3dcb8ff2726c893655969acae4f4
Patrik Piskay <piskay.patrik@gmail.com>
2016-08-20 23:03:30 +0100

    Initial setup

---
+++ src/GeckoOMeter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GeckoOMeter extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default GeckoOMeter;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GeckoOMeter from './GeckoOMeter';
+import './index.css';
+
+ReactDOM.render(
+  <GeckoOMeter />,
+  document.getElementById('root')
+);
